b41d8e5e15cf106624626645587c5e290fbaa9eb
Garrylou <113482854+Garrylou@users.noreply.github.com>
2026-01-29 09:13:02 -0500

    Update mission1.ts

--- mission1.ts
+++ mission1.ts
@@ -1,42 +1,27 @@
+
 /**
- * Mission 1 Extension
- * Adds a checkmark controller triggered by Button A
+ * Set the color of a namespace (block category) by setting
+ * the color attribute annotation on the namespace. Icons
+ * are drawn from https://semantic-ui.com/elements/icon.html
+ * To find the Unicode representation of an icon, inspect
+ * the element using the browser's debugger
  */
-//% weight=100 color="#FF8000" icon="\uf1eb" blockNamespace="Mission 1"
-namespace mission1 {
 
-    let pressCount = 0
-    let triggerPresses = 0
+//% color="#ff0000" icon="\uf0a4"
+namespace color {
 
-    /**
-     * Mission checkmark controller
-     */
-    //% block="Mission checkmark controller"
-    //% color="#FF8000" icon="\uf1eb"
-    export function missionCheckmark() {
+    //% block
+    export function foo() {
 
-        pressCount = 0
-        triggerPresses = randint(5, 8) // random presses to trigger
+    }
 
-        input.onButtonPressed(Button.A, function () {
-            pressCount++
+    //% block
+    export function foo1() {
 
-            if (pressCount >= triggerPresses) {
-                // Show checkmark randomly on the matrix
-                let x = randint(0, 4)
-                let y = randint(0, 4)
+    }
 
-                basic.clearScreen()
-                led.plot(x, y) // flash one LED first
-                basic.pause(100)
-                basic.showIcon(IconNames.Yes) // full checkmark
-                basic.pause(500)
-                basic.clearScreen()
+    //% block
+    export function foo2() {
 
-                // Reset counter
-                pressCount = 0
-                triggerPresses = randint(5, 8)
-            }
-        })
     }
 }
